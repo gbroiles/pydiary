dfba26145b7972c247b7c3f55aa9b22c9bb1005d
gbroiles <gbroiles@gmail.com>
2020-03-13 21:46:11 -0700

    small changes

--- d.py
+++ d.py
@@ -1,3 +1,32 @@
 import datetime
 import pynacl
 import dropbox
+import PySimpleGUI as sg
+
+def main():
+    """ main event loop """
+
+    layout1 = [
+            sg.Multiline(key="diary", size=(85, 40), autoscroll=False),
+    ]
+
+
+    layout = [
+
+        [sg.Button("Datestamp"), sg.Button("Save")],
+    ]
+
+    window = sg.Window(
+        "Diary",
+        layout,
+        return_keyboard_events=True,
+        grab_anywhere=False,
+                size=(650, 200),
+    )
+
+
+
+
+if __name__ == "__main__":
+    main()
+
