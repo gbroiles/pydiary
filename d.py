ddbc5bb1a0febc603918cec8a85f72622eeb65f4
gbroiles <gbroiles@gmail.com>
2020-03-14 11:31:13 -0700

    super basic text field stuff working

--- d.py
+++ d.py
@@ -1,32 +1,44 @@
 import datetime
-import pynacl
+import nacl
 import dropbox
 import PySimpleGUI as sg
+import pprint
+
 
 def main():
     """ main event loop """
 
-    layout1 = [
-            sg.Multiline(key="diary", size=(85, 40), autoscroll=False),
-    ]
-
-
     layout = [
-
-        [sg.Button("Datestamp"), sg.Button("Save")],
+        [
+            sg.Multiline(
+                key="diary", size=(85, 25), autoscroll=True, font=("Times", 12, "")
+            )
+        ],
+        [sg.Button("Datestamp"), sg.Button("Horizontal line"), sg.Button("Save")],
     ]
 
     window = sg.Window(
-        "Diary",
-        layout,
-        return_keyboard_events=True,
-        grab_anywhere=False,
-                size=(650, 200),
+        "Diary", layout, return_keyboard_events=True, grab_anywhere=False,
     )
 
+    diary = window.FindElement("diary")
 
+    while True:
+        event, values = window.read()
+        if event in (None, "Cancel"):
+            break
+        if event == "Datestamp":
+            now = datetime.datetime.now()
+            spacer = "=" * 6
+            stamp = (
+                "\n" + spacer + " [ " + now.strftime("%X %x") + " ] " + spacer + "\n"
+            )
+            diary.update(value=stamp, append=True)
+        if event == "Horizontal line":
+            diary.update(value=("\n" + "-" * 30 + "\n"), append=True)
+
+    window.close()
 
 
 if __name__ == "__main__":
     main()
-
